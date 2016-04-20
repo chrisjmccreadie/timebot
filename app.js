b5f6cb93a1c4bce4360b02a3a557366e3893373b
chris <chris@totemsoftware.co.uk>
2016-04-21 00:10:22 +0100

    clean up of tasks module and addition of a config file

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express');
+//load the config file.
+var nconf = require('nconf');
 var app = express();
+// First consider commandline arguments and environment variables, respectively.
+nconf.argv().env();
+// Then load configuration from a designated file.
+nconf.file({ file: './config/'+app.get('env')+'.json' });
+// Provide default values for settings not provided above.
+nconf.defaults({
+    'http': {
+        'port': 3000
+    }
+});
 
 
 app.get('/', function (req, res) {
   res.send('Hello World I am time bot');
 });
+
 
 //call in the tasks module
 var tasks = require('./tasks');
@@ -15,6 +28,6 @@ var user = require('./user');
 app.use('/user', user);
 
 //listen for connections
-app.listen(3000, function () {
+app.listen(nconf.get('http:port'), function () {
   console.log('Hello I am timebot!');
 });
--- tasks.js
+++ tasks.js
@@ -1,6 +1,9 @@
 
 //include all the things
 var express = require('express');
+//load the config file.
+var nconf = require('nconf');
+
 var router = express.Router();
 var r = require('rethinkdb');
 //note (chris) there is no requirement for these to be global.
@@ -19,74 +22,7 @@ r.connect( {host: 'localhost', port: 28015,database:'timebot'}, function(err, co
 })
 
 
-//note (chris ) this seemds odd that I require a public function, like this research the call back or the chainging of rethinkdb
-function setSubmitId(result,res)
-{
-	//this is the balance amount to add to the users account, this is set to one initially 
-	//note (chris) when we get to the machine learning fun then we we will set this based on the quality of the task added
-	//			   and modifiers for the reputation of the user.
-	var balanceamount = 1;
 
-	//check the count.
-	if (result == 0)
-	{
-		submiterclaimid = submitterid;
-		submitterid = 0;
-
-	}
-	else
-	{
-		submiterclaimid =0;
-	}
-
-
-	//add a record based on the names
-  	//note (chris) make this check the min paramaters are in place. 
-  	r.db('timebot').table('tasks').insert([
-    	   { name: taskname,submitterid: submitterid,submiterclaimid:submiterclaimid}
-    	  
-	]).run(connection, function(err, result) 
-	{
-		//oh on something done gone wrong.
-
-      	if (err) throw err;
-    		console.log(JSON.stringify(result, null, 2));
-
-    	//output it to the screen.
-    	var taskid = result.generated_keys[0];
-    	//return the task id.  So they user can come and retiever it later.  This could be useful if you wanted to do things such as
-    	//					   Allow the user maintain ip / copyright on the task submitted 
-    	//					   Allow the user to delete it, you would have to factor in the payment part here, payment should at the very least give a read only on the task forever for timebot
-    	//					   Allow the user to see how many times it has been used and how inflenutial this commit has been to the overall knowledge pool
-    	//					   Allow the user to specify in which context the task is allowed to be user "open source queries only" for instance. 	 
-  		res.json({'status':'Added',taskid:taskid});
-	})
-
-	//we have to update the user balance, they gave us data they deserve.
-	if (submitterid != 0)
-	{
-		//this is so cool, easy to add one and if the field is not there sets it to 1.  Go nosql!!!!
-		r.db('timebot').table("users").get(submitterid).update({
-			//note (chris) use balance amount here when we get round to it.
-    		balance: r.row("balance").add(1).default(1),
-    		taskssubmitted: r.row("taskssubmitted").add(1).default(1)
-		}).run(connection);
-
-
-		//if user not found set the sumbitterid to 0 and set the claimid to the id submitted.
-		
-	}
-	else
-	{
-		//they either specifed it as anon or we could not find the key either it is added to the charity balance
-		//note (chris) we may want to log the time for each of these charity additions as at some point in the future a user could come 
-		//			   in and claim a block taking the charity balance into the minus, which is no good at all. 		
-		
-		r.db('timebot').table("charity").get('f7cfcee1-b3f3-4922-b05d-54897f3b48f7').update({balance: r.row("balance").add(1).default(1)}).run(connection);
-		
-
-	}
-}
 
 /*
 * This function processes the add task 
@@ -107,11 +43,68 @@ function processAddTask(req,res)
     run(connection, function(err, result) 
     {
 	    if (err) throw err;
-		
-		//note (Chris) I am calling an external function because I could not get access to the req function in this var, it seems very odd
-		//			   this would be the case I am surely doing something wrong here. I will refactor this later when I have more knowledge
-		//			   of how Node.js does this kind of thing.
-		setSubmitId(result,res);
+	    
+	    //this is the balance amount to add to the users account, this is set to one initially 
+		//note (chris) when we get to the machine learning fun then we we will set this based on the quality of the task added
+		//			   and modifiers for the reputation of the user.
+		var balanceamount = 1;
+		if (result == 0)
+		{
+			submiterclaimid = submitterid;
+			submitterid = 0;
+
+		}
+		else
+		{
+			submiterclaimid =0;
+		}
+
+		//add a record based on the names
+	  	//note (chris) make this check the min paramaters are in place. 
+	  	r.db('timebot').table('tasks').insert([
+	    	   { name: taskname,submitterid: submitterid,submiterclaimid:submiterclaimid}
+	    	  
+		]).run(connection, function(err, result) 
+		{
+			//oh on something done gone wrong.
+
+	      	if (err) throw err;
+	    		console.log(JSON.stringify(result, null, 2));
+
+	    	//output it to the screen.
+	    	var taskid = result.generated_keys[0]; 
+	    	//return the task id.  So they user can come and retiever it later.  This could be useful if you wanted to do things such as
+	    	//					   Allow the user maintain ip / copyright on the task submitted 
+	    	//					   Allow the user to delete it, you would have to factor in the payment part here, payment should at the very least give a read only on the task forever for timebot
+	    	//					   Allow the user to see how many times it has been used and how inflenutial this commit has been to the overall knowledge pool
+	    	//					   Allow the user to specify in which context the task is allowed to be user "open source queries only" for instance. 	 
+	  		res.json({'status':'Added',taskid:taskid});
+		})
+
+		//we have to update the user balance, they gave us data they deserve.
+		if (submitterid != 0)
+		{
+			//this is so cool, easy to add one and if the field is not there sets it to 1.  Go nosql!!!!
+			r.db('timebot').table("users").get(submitterid).update({
+				//note (chris) use balance amount here when we get round to it.
+	    		balance: r.row("balance").add(1).default(1),
+	    		taskssubmitted: r.row("taskssubmitted").add(1).default(1)
+			}).run(connection);
+
+
+			//if user not found set the sumbitterid to 0 and set the claimid to the id submitted.
+			
+		}
+		else
+		{
+			//they either specifed it as anon or we could not find the key either it is added to the charity balance
+			//note (chris) we may want to log the time for each of these charity additions as at some point in the future a user could come 
+			//			   in and claim a block taking the charity balance into the minus, which is no good at all. 		
+			
+			r.db('timebot').table("charity").get('f7cfcee1-b3f3-4922-b05d-54897f3b48f7').update({balance: r.row("balance").add(1).default(1)}).run(connection);
+			
+
+		}
 
 	});
 
@@ -120,12 +113,12 @@ function processAddTask(req,res)
 
 // define the  add task route
 // note (chris) we could link these routes together instead of having the get and post toghether, research how.
-router.get('/add/:submitterid/:taskname', function(req, res) {
+router.get('/:submitterid/:taskname', function(req, res) {
 	processAddTask(req,res);
 });
 
 // define the  add task route
-router.post('/add/:submitterid/:taskname', function(req, res) {
+router.post('/:submitterid/:taskname', function(req, res) {
 	processAddTask(req,res);
 });
 // define the about route
